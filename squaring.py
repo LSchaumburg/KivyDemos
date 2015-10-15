f48fdcea8d3d66c8eb40a91a4280284ca95177cf
LSchaumburg <luke.schaumburg@my.jcu.edu.au>
2015-10-15 13:06:23 +1000

    Changed window size

--- squaring.py
+++ squaring.py
@@ -8,14 +8,14 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
-__author__ = 'Lindsay Ward'
+__author__ = 'Lindsay Ward and jc247746'
 
 
 class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 100)
+        Window.size = (400, 200)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
